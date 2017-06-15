72792c248872ed37ce2070ec2f92e3d5faadd564
Baldwin Martinez <baldwin.a.martinez@gmail.com>
2017-06-15 15:45:52 -0400

    Added styles, scripts and assets for: - CTRL-49 - M3-8

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -426,6 +426,171 @@ $('.reveal-overlay').on('click', function() {
     shares: ['facebook', 'twitter']
   });
 
+
+  /*
+   |--------------------------------------------------------------------------
+   |                     M3-8 Product Microsite
+   |--------------------------------------------------------------------------
+   */
+
+
+  $('#m3-8-hero').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 10000,
+    cssEase: 'linear',
+    arrows: false
+  });
+
+  $('#m3-8-hero-mobile').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 10000,
+    cssEase: 'linear',
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          dots: false
+        }
+      }
+    ]
+  });
+
+
+  $('#m3-8-summary').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    dots: true,
+    autoplaySpeed: 10000,
+    responsive:[
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+
+
+
+  /*
+   |--------------------------------------------------------------------------
+   |                     CTRL-49 Product Microsite
+   |--------------------------------------------------------------------------
+   */
+
+
+  $('div#ctrl-49-main-slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    autoplay: false,
+    fade: true,
+    speed: 500
+  });
+
+
+
+  $('.overview-video-selection').slick({
+    autoplay: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    fade: false,
+    speed: 500,
+    arrows: true,
+    asNavFor: '.overview-video-selector',
+    infinite: false
+  }).on('beforeChange', function(event, slick, currentSlide){
+
+    pauseYouTubeVideos();
+
+  });
+
+
+  $('.overview-video-selector').slick({
+    autoplay: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    dots: false,
+    asNavFor: '.overview-video-selection',
+    focusOnSelect: true,
+    infinite: false,
+    arrows: true
+  });
+
+
+
+  $('#software-include-slider').slick({
+    autoplay: false,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    variableWidth:  false,
+    arrows: false,
+    dots: true,
+    infinite: false,
+
+    responsive: [
+      {
+        breakpoint: 769,
+        settings:{
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false,
+          // infinite: true,
+          autoplay: true,
+          speed: 400,
+          autoplaySpeed: 3000
+        }
+      }
+    ]
+  });
+
+
+  $('.video-with-cover-container .video-cover').on('click', function(){
+    var playerContainer = $(this).closest('.video-with-cover-container');
+    var youtubeVideo = playerContainer.children('.video-player')[0];
+    var playerId = $(youtubeVideo).attr('id');
+    var player = YT.get(playerId);
+
+
+    playerContainer.addClass('video-started');
+
+    pauseYouTubeVideos();
+
+    player.playVideo()
+  });
+
+
+  $('.box-wrapper.collapsible').on('click',function(e){
+
+    console.log('Collapse/Expand!');
+
+    console.log('Target: ', e.currentTarget);
+    $(e.currentTarget).toggleClass('show');
+  });
+
 /*
 |--------------------------------------------------------------------------
 |                                 BX5
@@ -473,6 +638,66 @@ $('.reveal-overlay').on('click', function() {
   $('#bx5-lightgallery').lightGallery({
     selector: 'a',
     mode: 'lg-fade'
+  });
+
+
+  /*
+   |--------------------------------------------------------------------------
+   |                                 Hammer88
+   |--------------------------------------------------------------------------
+   */
+
+
+  $('#hammer88-overview').on('click', function() {
+    $('.bx5-tabs-left-bracket').hide();
+    $('.bx5-tabs-right-bracket').hide();
+    $('.overview-left-bracket').show();
+    $('.overview-right-bracket').show();
+  });
+
+  $('#hammer88-features').on('click', function() {
+    $('.bx5-tabs-left-bracket').hide();
+    $('.bx5-tabs-right-bracket').hide();
+    $('.features-left-bracket').show();
+    $('.features-right-bracket').show()
+  });
+
+  $('#hammer88-videos-photos').on('click', function() {
+    $('.bx5-tabs-left-bracket').hide()
+    $('.bx5-tabs-right-bracket').hide()
+    $('.media-left-bracket').show()
+    $('.media-right-bracket').show()
+  });
+
+  $('#hammer88-specs').on('click', function(){
+    $('.bx5-tabs-left-bracket').hide()
+    $('.bx5-tabs-right-bracket').hide()
+    $('.specs-left-bracket').show()
+    $('.specs-right-bracket').show()
+  });
+
+  $('.hammer88-specs').hide();
+  $('.hammer88-videos-photos').hide();
+  $('.hammer88-features').hide();
+
+  $('#hammer88-overview').on('click', function() {
+    $('.product-content-jquery').fadeOut('fast');
+    $('.hammer88-overview').fadeIn('slow');
+  })
+
+  $('#hammer88-specs').on('click', function() {
+    $('.product-content-jquery').fadeOut('fast');
+    $('.hammer88-specs').fadeIn('slow');
+  });
+
+  $('#hammer88-videos-photos').on('click', function() {
+    $('.product-content-jquery').fadeOut('fast');
+    $('.hammer88-videos-photos').fadeIn('slow');
+  });
+
+  $('#hammer88-features').on('click', function(){
+    $('.product-content-jquery').fadeOut('fast');
+    $('.hammer88-features').fadeIn('slow');
   });
 
 }); // CLOSING OF $(function() {
